f784b5ff4e32de5354283cc28a5d3649061b9c46
Harsha S M <harshasreeramharshasm2306@gmail.com>
2025-07-22 15:10:34 +0530

    Refactored to increase readability

--- src/main/java/vitals/VitalsChecker.java
+++ src/main/java/vitals/VitalsChecker.java
@@ -2,34 +2,27 @@ package vitals;
 
 
 public abstract class VitalsChecker {
+
+  static void displayError(String message){
+    System.out.println(message);
+    for (int i = 0; i < 6; i++) {
+        System.out.print("\r* ");
+        Thread.sleep(1000);
+        System.out.print("\r *");
+        Thread.sleep(1000);
+      }
+  }
+
   static boolean vitalsOk(float temperature, float pulseRate, float spo2) 
       throws InterruptedException {
     if (temperature > 102 || temperature < 95) {
-      System.out.println("Temperature is critical!");
-      for (int i = 0; i < 6; i++) {
-        System.out.print("\r* ");
-        Thread.sleep(1000);
-        System.out.print("\r *");
-        Thread.sleep(1000);
-      }
+      displayError("Temperature is critical!");
       return false;
     } else if (pulseRate < 60 || pulseRate > 100) {
-      System.out.println("Pulse Rate is out of range!");
-      for (int i = 0; i < 6; i++) {
-        System.out.print("\r* ");
-        Thread.sleep(1000);
-        System.out.print("\r *");
-        Thread.sleep(1000);
-      }
+      displayError("Pulse Rate is out of range!");
       return false;
     } else if (spo2 < 90) {
-      System.out.println("Oxygen Saturation out of range!");
-      for (int i = 0; i < 6; i++) {
-        System.out.print("\r* ");
-        Thread.sleep(1000);
-        System.out.print("\r *");
-        Thread.sleep(1000);
-      }
+      displayError("Oxygen Saturation out of range!");
       return false;
     }
     return true;
